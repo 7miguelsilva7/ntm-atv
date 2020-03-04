33f41dedef903f05bda6a5aaa91c6f71713b21f2
7miguelsilva7 <7miguelsilva7@gmail.com>
2020-03-04 16:55:53 -0300

    updated 2020-03-04

--- gerarSite.sh
+++ gerarSite.sh
@@ -3,8 +3,10 @@ hugo
 cp *.html public/
 
 cp themes/slate/layouts/partials/header.html public/
+cp -r atividades public/
+cp -r Objetos public/
 cd public/
 cat header.html index.html > index2.html
 rm header.html
 mv index2.html index.html
-mv 404.html disc.html
+#mv 404.html disc.html
